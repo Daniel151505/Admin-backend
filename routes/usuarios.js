339cf0582c8516b2a7712b9ca50c755f72a718cf
Daniel151505 <daniel150520011@gmail.com>
2022-07-29 22:42:30 -0500

    Revisión del token Middleware

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -6,9 +6,10 @@ const { validarCampos } = require('../middlewares/validar-campos');
 
 
 const { getUsuarios, crearUsuario, actualizarUsuario, borrarUsuario } = require('../controllers/usuarios');
+const { validarJWT } = require('../middlewares/validar-jwt');
 const router = Router();
 
-router.get( '/', getUsuarios );
+router.get( '/', validarJWT, getUsuarios );
 
 router.post( '/',
     [
@@ -22,15 +23,17 @@ router.post( '/',
 
 router.put( '/:id',
     [
+        validarJWT,
         check('nombre', 'El nombre es obligatorio').not().isEmpty(),
         check('email', 'El email es obligatorio').isEmail(),
         check('role', 'El role es obligatorio').not().isEmpty(),
-        validarCampos
+        validarCampos,
     ],
     actualizarUsuario 
 );
 
 router.delete( '/:id',
+    validarJWT,
     borrarUsuario
 );
 
