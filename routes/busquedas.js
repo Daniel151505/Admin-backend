67734c6a53e00485f34c3eceeceeb62cdc8f0385
Daniel151505 <daniel150520011@gmail.com>
2022-08-27 13:57:53 -0500

    Búsqueda por colección específica

--- routes/busquedas.js
+++ routes/busquedas.js
@@ -6,11 +6,13 @@
 const { Router } = require('express');
 const { validarJWT } = require('../middlewares/validar-jwt')
 
-const { getTodo } = require('../controllers/busquedas');
+const { getTodo, getDocumentosColeccion } = require('../controllers/busquedas');
 
 const router = Router();
 
 router.get('/:busqueda', validarJWT, getTodo)
+
+router.get('/coleccion/:tabla/:busqueda', validarJWT, getDocumentosColeccion)
 
 
 module.exports = router;
