cb672f311a4a063b5675c4ea2cae1ec18bc012aa
Daniel151505 <daniel150520011@gmail.com>
2022-08-28 14:29:15 -0500

    Terminando la actualización de imagenes

--- helpers/actualizar-imagen.js
+++ helpers/actualizar-imagen.js
@@ -4,7 +4,16 @@ const Usuario = require('../models/usuario');
 const Medico = require('../models/medico');
 const Hospital = require('../models/hospital');
 
+const borrarImagen = ( path ) => {
+    if ( fs.existsSync( path ) ) {
+        // borrar la imagen anterior
+        fs.unlinkSync( path );
+    }
+}
+
 const actualizarImagen = async(tipo, id , nombreArchivo) => {
+
+    let pathViejo = '';
 
     switch ( tipo ) {
         case 'medicos':
@@ -14,11 +23,8 @@ const actualizarImagen = async(tipo, id , nombreArchivo) => {
                 return false;
             }
 
-            const pathViejo = `./uploads/medicos/${ medico.img }`;
-            if ( fs.existsSync( pathViejo ) ) {
-                // borrar la imagen anterior
-                fs.unlinkSync( pathViejo );
-            }
+            pathViejo = `./uploads/medicos/${ medico.img }`;
+            borrarImagen(pathViejo);
 
             medico.img = nombreArchivo;
             await medico.save();
@@ -27,11 +33,34 @@ const actualizarImagen = async(tipo, id , nombreArchivo) => {
         break;
 
         case 'hospitales':
-            
+            const hospital = await Hospital.findById(id);
+            if ( !hospital ) {
+                console.log('No es un hospital por id')
+                return false;
+            }
+
+            pathViejo = `./uploads/hospitales/${ hospital.img }`;
+            borrarImagen(pathViejo);
+             
+            hospital.img = nombreArchivo;
+            await hospital.save();
+            return true;
+
         break;
 
         case 'usuarios':
-            
+            const usuario = await Usuario.findById(id);
+            if ( !usuario ) {
+                console.log('No es un usuario por id')
+                return false;
+            }
+
+            pathViejo = `./uploads/usuarios/${ usuario.img }`;
+            borrarImagen(pathViejo);
+             
+            usuario.img = nombreArchivo;
+            await usuario.save();
+            return true;
         break;
     
         default:
