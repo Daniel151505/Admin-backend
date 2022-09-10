0dcf839f6fac21366d9361838b37012427cc4e0a
Daniel151505 <daniel150520011@gmail.com>
2022-09-09 22:44:00 -0500

    Creando registro en la colección de usuarios

--- controllers/auth.js
+++ controllers/auth.js
@@ -54,9 +54,33 @@ const googleSignIn = async( req, res = response ) => {
     
     try {
         const { email, name, picture }  = await googleVerify( req.body.token );
+
+        const usuarioDB = await Usuario.findOne({ email });
+        let usuario;
+
+        if ( !usuarioDB ) {
+            usuario = new Usuario({
+                nombre: name,
+                email,
+                password: '@@@',
+                img: picture,
+                google: true
+            })
+        } else {
+            usuario = usuarioDB;
+            usuario.google = true;
+        }
+
+        //Guardar Usuario
+        await usuario.save();
+
+        //Generar el TOKEN - JWT
+        const token = await generarJWT(usuario.id);
+
         res.json({ 
             ok: true,
-            email, name, picture
+            email, name, picture,
+            token
         })
     } catch (error) {
         console.log(error);
